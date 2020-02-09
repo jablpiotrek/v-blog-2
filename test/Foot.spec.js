50a6cf570e751a05a389afa78fb4997344fd45cd
Piotr Jablonski <jabl.piotrek@gmail.com>
2020-02-09 21:13:03 +0100

    update tests

--- test/Foot.spec.js
+++ test/Foot.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Foot from '@/components/Foot.vue'
 
-describe('Logo', () => {
+describe('Foot', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Foot)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })

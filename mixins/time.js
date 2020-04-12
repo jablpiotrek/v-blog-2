fc66fadb1f64eb082454982f6e2412de4527301a
Piotr Jablonski <jabl.piotrek@gmail.com>
2020-04-12 21:18:54 +0200

    add date-fns

--- mixins/time.js
+++ mixins/time.js
@@ -1,14 +1,12 @@
+import { format } from 'date-fns'
+
 const time = {
   methods: {
-    time () {
-      const date = new Date()
-      const day = date.getDate() >= 10 ? date.getDate() : `0${date.getDate()}`
-      const month = date.getMonth() + 1 >= 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`
-      const year = date.getFullYear()
-      const hours = date.getHours() >= 10 ? date.getHours() : `0${date.getHours()}`
-      const minutes = date.getMinutes() >= 10 ? date.getMinutes() : `0${date.getMinutes()}`
-
-      return `${day}/${month}/${year} ${hours}:${minutes}`
+    timestamp () {
+      return Date.now()
+    },
+    formatTime (timestamp) {
+      return format(timestamp, 'dd/MM/yyyy HH:mm')
     }
   }
 }
